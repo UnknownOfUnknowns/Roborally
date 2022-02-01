a38ddbf9006debe38db477102c2af3c0dd668a9a
Hurtigpilskade <albertmatthaihalkjaer@gmail.com>
2022-02-01 15:58:51 +0100

    Controller comments

---
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/controller/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * The game controller is present as a controller. It calls the different methods throughout the code.
+ * This means that the class does all the functional work. It tells us which directions we can go, which command cards
+ * we get.
+ *
+ *
+ * The app controller is used primarily for estetics. Meaning we wouldn't have an actual picture on the screen.
+ * So in the app controller we define which colors we use for the players and how we save/load/stop a game*/
+package dk.dtu.compute.se.pisd.roborally.controller;
