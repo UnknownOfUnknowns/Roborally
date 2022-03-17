0299cef8b0f6fd17576f384dec4a41c702b5b257
minah <s215722@student.dtu.dk>
2022-03-17 19:26:52 +0100

    energy

--- src/main/java/dk/dtu/compute/se/pisd/roborally/controller/AppController.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/controller/AppController.java
@@ -56,6 +56,7 @@ public class AppController implements Observer {
     final private List<Integer> PLAYER_NUMBER_OPTIONS = Arrays.asList(2, 3, 4, 5, 6);
     final private List<String> PLAYER_COLORS = Arrays.asList("red", "green", "blue", "orange", "grey", "magenta");
 
+
     final private RoboRally roboRally;
 
     private GameController gameController;
--- src/main/java/dk/dtu/compute/se/pisd/roborally/model/Player.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/Player.java
@@ -22,6 +22,7 @@
 package dk.dtu.compute.se.pisd.roborally.model;
 
 import dk.dtu.compute.se.pisd.designpatterns.observer.Subject;
+import dk.dtu.compute.se.pisd.roborally.controller.AppController;
 import org.jetbrains.annotations.NotNull;
 
 import static dk.dtu.compute.se.pisd.roborally.model.Heading.SOUTH;
@@ -41,6 +42,7 @@ public class Player extends Subject {
 
     private String name;
     private String color;
+    private int energy;
 
     private Space space;
     private Heading heading = SOUTH;
@@ -142,4 +144,13 @@ public class Player extends Subject {
     public void setCheckpointsReached(int checkpointsReached) {
         this.checkpointsReached = checkpointsReached;
     }
+    public void setEnergy(int energy){
+        this.energy=energy;
+    }
+
+    public int energy(){
+        return energy;
+    }
+
+
 }
