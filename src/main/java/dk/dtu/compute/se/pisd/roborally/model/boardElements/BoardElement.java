1c6422d1e203b6b5d51af849aafa83d178bf462e
Unknown Unknown <hans@bilstrupandersen.dk>
2022-03-21 17:26:01 +0100

    Javadoc added to BoardElement interface

--- src/main/java/dk/dtu/compute/se/pisd/roborally/model/boardElements/BoardElement.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/boardElements/BoardElement.java
@@ -5,6 +5,9 @@ import dk.dtu.compute.se.pisd.roborally.model.Player;
 import dk.dtu.compute.se.pisd.roborally.model.Space;
 
 public interface BoardElement {
-
+    /**
+     * @author s215705
+     * This method is called whenever a player lands on a space with a boardelement
+     * */
     void interact(Player player);
 }
