ad1ac40b113415281102d91fccd27bcfb2b2095c
MartinFilrup <martinfilrup@gmail.com>
2022-03-21 18:27:54 +0100

    Unit Test for energySpace

---
+++ src/test/java/dk/dtu/compute/se/pisd/roborally/Space/EnergySpaceTest.java
@@ -0,0 +1,51 @@
+package dk.dtu.compute.se.pisd.roborally.Space;
+
+import dk.dtu.compute.se.pisd.roborally.controller.GameController;
+import dk.dtu.compute.se.pisd.roborally.model.*;
+import dk.dtu.compute.se.pisd.roborally.model.boardElements.ConveyerBelt;
+import dk.dtu.compute.se.pisd.roborally.model.boardElements.EnergySpace;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class EnergySpaceTest {
+    private final int TEST_WIDTH = 8;
+    private final int TEST_HEIGHT = 8;
+
+    private GameController gameController;
+
+    @BeforeEach
+    void setUp() {
+        Board board = new Board(TEST_WIDTH, TEST_HEIGHT);
+        gameController = new GameController(board);
+        for (int i = 0; i < 6; i++) {
+            Player player = new Player(board, null,"Player " + i);
+            board.addPlayer(player);
+            player.setSpace(board.getSpace(i, i));
+            player.setHeading(Heading.values()[i % Heading.values().length]);
+        }
+        board.setCurrentPlayer(board.getPlayer(0));
+        board.getSpace(0,1).setBoardElement(new EnergySpace());
+    }
+    @Test
+    void useEnergySpace(){
+        Board board = gameController.board;
+        board.setPhase(Phase.ACTIVATION);
+        Player current = board.getCurrentPlayer();
+        current.getProgramField(0).setCard(new CommandCard(Command.FORWARD));
+        current.getProgramField(1).setCard(new CommandCard(Command.FORWARD));
+        int oldEnergy = current.getEnergyCubes();
+        gameController.executePrograms();
+        Assertions.assertEquals(oldEnergy+1, current.getEnergyCubes(), "The player should be on the space following the ConveyerBelt");
+    }
+    @Test
+    void receivesEnergySpaceFromBank(){
+        Board board = gameController.board;
+        board.setPhase(Phase.ACTIVATION);
+        Player current = board.getCurrentPlayer();
+        current.getProgramField(0).setCard(new CommandCard(Command.FORWARD));
+        int oldEnergy = current.getEnergyCubes();
+        gameController.executePrograms();
+        Assertions.assertEquals(oldEnergy+2, current.getEnergyCubes(), "The player should be on the space following the ConveyerBelt");
+    }
+}
